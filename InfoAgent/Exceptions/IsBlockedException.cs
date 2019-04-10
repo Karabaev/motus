744bd8d906c4c47ef0d1e6a31ce95ca385799a43
RomanChernyshev <aartman@rambler.ru>
2019-04-10 22:02:30 +0300

    MOTUS-11 Заигнорить все фильмы, которые нельзя посмотреть

---
+++ InfoAgent/Exceptions/IsBlockedException.cs
@@ -0,0 +1,26 @@
+﻿namespace InfoAgent.Exceptions
+{
+    using System;
+
+    class IsBlockedException: Exception
+    {
+        public string Value { get; }
+
+        public IsBlockedException()
+        {
+
+        }
+
+        public IsBlockedException(string message)
+            : base(message)
+        {
+
+        }
+
+        public IsBlockedException(string message, string value)
+            : base(message)
+        {
+            this.Value = value;
+        }
+    }
+}
