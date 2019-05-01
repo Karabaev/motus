efd24cb806d98465d43f565dfc1235484c271335
RomanChernyshev <aartman@rambler.ru>
2019-05-02 00:56:43 +0300

    MOTUS-76 Сделать красивую вьюшку авторизации fixed

--- SerialService/Scripts/AccountForms/CommonForm.js
+++ SerialService/Scripts/AccountForms/CommonForm.js
@@ -13,16 +13,25 @@ function HideErrorMessage() {
 }
 
 function SubmitButtonManage() {
-    var fieldsIsValid = document.getElementsByClassName('is-invalid').length === 0
-    var button = document.getElementById('submit-btn')
-    var attr = 'disabled';
-    var battonIsDisabled = button.hasAttribute(attr);
-    if (battonIsDisabled && fieldsIsValid) {
-        button.removeAttribute(attr)
-    }
-    else if (!battonIsDisabled && !fieldsIsValid) {
-        button.setAttribute(attr, attr)
-    }
+    setTimeout(function () {
+        var fieldsIsValid = document.getElementsByClassName('is-invalid').length === 0;
+        var requieredFields = document.getElementsByClassName('required-field');
+        Array.prototype.forEach.call(requieredFields, function (field) {
+            console.log(field.value);
+            if (!field.value) {
+                fieldsIsValid = false;
+            }
+        });
+        var button = document.getElementById('submit-btn');
+        var attr = 'disabled';
+        var battonIsDisabled = button.hasAttribute(attr);
+        if (battonIsDisabled && fieldsIsValid) {
+            button.removeAttribute(attr)
+        }
+        else if (!battonIsDisabled && !fieldsIsValid) {
+            button.setAttribute(attr, attr)
+        }
+    }, 50)    
 }
 
 function ValidatePassword() {
