45575eec620a69b2df6234dd6e7373677503c25d
Karabas <maxakarab@yandex.ru>
2019-06-12 20:37:11 +0400

    Добавил классы сущностей комментов

---
+++ SerialService/DAL/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿namespace SerialService.DAL.Entities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Comment : IBaseEntity
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public string AuthorID { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+        public int ParentID { get; set; }
+        public Comment Parent { get; set; }
+        public List<Comment> DependentComments { get; set; }
+        public List<CommentMark> Marks { get; set; }
+
+        public bool Alike(IBaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
