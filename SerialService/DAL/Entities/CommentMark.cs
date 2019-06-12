758ea3e9b01d84543206f05945ef2efdb19cfea7
Karabas <maxakarab@yandex.ru>
2019-06-12 20:37:11 +0400

    Добавил классы сущностей комментов

---
+++ SerialService/DAL/Entities/CommentMark.cs
@@ -0,0 +1,25 @@
+﻿namespace SerialService.DAL.Entities
+{
+    using System;
+
+    public class CommentMark : IBaseEntity
+    {
+        public int ID { get; set; }
+        public string UserIP { get; set; }
+        public bool Value { get; set; }
+        public int CommentID { get; set; }
+        public virtual Comment Comment { get; set; }
+        public string AuthorID { get; set; }
+        public ApplicationUser Author { get; set; }
+
+        /// <summary>
+        /// Проверяет на равенство объекты по главным свойствам. (VideoMaterialID и UserIP)
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool Alike(IBaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
