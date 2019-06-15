0525647ce227c9fbfca45d62a399b91fad75c372
Karabas <maxakarab@yandex.ru>
2019-06-15 13:57:03 +0400

    Добавил файлы миграций. Добавил обработку исключения при заполнении IFrameUrl

--- Tools/Modules/IFrameUrlInitializerModule.cs
+++ Tools/Modules/IFrameUrlInitializerModule.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tools.Modules
 {
+    using System;
     using InfoAgent;
     using InfoAgent.Moonwalk;
     using SerialService.DAL;
@@ -11,25 +12,33 @@
         {
             foreach (var item in unitOfWork.VideoMaterials.GetAll())
             {
-                this.logger.Info("Проверка видеоматериала с ID {0} Title {1}", item.ID, item.Title);
-
-                if (string.IsNullOrWhiteSpace(item.IframeUrl))
+                try
                 {
-                    this.logger.Info("Загрузка информации о фильме с Кинопоиск ID {0}", item.KinopoiskID);
-                    FilmInfo info = this.service.GetFilmInfo(item.KinopoiskID);
-                    this.logger.Info("Информация о фильме с Кинопоиск ID {0} загружена", item.KinopoiskID);
+                    this.logger.Info("Проверка видеоматериала с ID {0} Title {1}", item.ID, item.Title);
 
-                    if (info != null)
+                    if (string.IsNullOrWhiteSpace(item.IframeUrl))
                     {
-                        this.logger.Info("Получен iframeUrl {0}", info.IframeUrl);
-                        item.IframeUrl = info.IframeUrl;
+                        this.logger.Info("Загрузка информации о фильме с Кинопоиск ID {0}", item.KinopoiskID);
+                        FilmInfo info = this.service.GetFilmInfo(item.KinopoiskID);
+                        this.logger.Info("Информация о фильме с Кинопоиск ID {0} загружена", item.KinopoiskID);
 
-                        if (unitOfWork.VideoMaterials.Update(item.ID, item))
-                            this.logger.Info("Видеоматериал изменен");
-                        else
-                            this.logger.Warn("Видеоматериал НЕ изменен");
+                        if (info != null)
+                        {
+                            this.logger.Info("Получен iframeUrl {0}", info.IframeUrl);
+                            item.IframeUrl = info.IframeUrl;
+
+                            if (unitOfWork.VideoMaterials.Update(item.ID, item))
+                                this.logger.Info("Видеоматериал изменен");
+                            else
+                                this.logger.Warn("Видеоматериал НЕ изменен");
+                        }
                     }
                 }
+                catch(Exception ex) 
+                {
+                    this.logger.Error(ex, "Ошибка при обновлении IframeUrl у фильма с ID {0}", item.ID);
+                }
+                
             }
         }
 
