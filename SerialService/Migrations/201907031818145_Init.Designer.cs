7ef0f5dae52a5322d34523227a6903274899bff8
Karabas <maxakarab@yandex.ru>
2019-07-03 22:25:32 +0400

    Создал инициализирующую миграцию

--- SerialService/Migrations/201907031818145_Init.Designer.cs
+++ SerialService/Migrations/201907031818145_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace SerialService.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class FilmsMigration : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(FilmsMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201906150738437_FilmsMigration"; }
+            get { return "201907031818145_Init"; }
         }
         
         string IMigrationMetadata.Source
